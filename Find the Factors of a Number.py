797f01cd6b9a60794ffdf6596fce48984b46f77d
Nikhil Pratap Singh <pratapnikhil106@gmail.com>
2022-03-16 16:13:47 +0530

    Factors of the number

---
+++ Find the Factors of a Number.py
@@ -0,0 +1,13 @@
+#Find the Factors of a Number
+
+def factors(a):
+    qaaa= []
+    for i in range (1, a+1):
+        if a % i ==0:
+            qaaa.append(i)
+     print (qaaa)
+
+    
+
+e = 42
+factors(e)
